d31005ed3900962fddda8b4e62853f76a6b4f949
RomaGilyov <rgilyov@gmail.com>
2017-05-15 10:09:31 +0300

    version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 setup(
     name='RGPageRank',
 
-    version='0.0.6 dev',
+    version='0.0.7 dev',
 
     description='Find the most relevant information from the given collection of data.',
 
